4caf4960fa080c4760e81bdadaa70a2bcb912354
Zoe <zmb6893@g.rit.edu>
2025-08-26 12:26:01 -0700

    Removed the http setup

--- Project/Code/Backend/server.js
+++ Project/Code/Backend/server.js
@@ -1,21 +1,22 @@
 // Imports
 const express = require('express');
-const http = require('http');
 
 // Server setup
-const serverCongifuration = {
+const serverConfiguration = {
 	hostname: '127.0.0.1',
 	port: 3000
 };
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Server created...');
-})
-
-server.listen(serverCongifuration.port, serverCongifuration.hostname, () => {
-	console.log(`Server running at http://${serverCongifuration.hostname}:${serverCongifuration.port}/`)
-})
+const app = express();
 
 // Endpoint setup
+app.get('/patient', (req, res) => {
+	res.send('This is the patient data...')
+})
+
+app.listen(serverConfiguration.port, () => {
+	console.log(`Server running at ${serverConfiguration.hostname}:${serverConfiguration.port}/`)
+}) 
+
+
+// Fire.ly data retrieval
